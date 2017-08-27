93a3f2032f5c7a8c79af338cd416cc5f23e03ef9
Damian Baran <damian.baran@t-systems.com>
2017-08-27 05:31:46 +0200

    some changes

--- html-parser-test.py
+++ html-parser-test.py
@@ -6,24 +6,10 @@ import lxml.html
 url = "https://www.e-obce.sk/obec/trnovo/trnovo.html"
 html = urllib2.urlopen(url).read()
 
-namespaces = {"ns": "http://schemas.xmlsoap.org/wsdl/"}
+root = lxml.html.fromstring(html)
 
-root = lxml.html.fromstring(html.decode('windows-1250'))
-
-print
-
-print "Root tag:"
-print(root.tag)
-
-print
-
-print "Count of elements under root tree:"
-print(len(root))
-
-print
-print
-
-print "List of elements based on XPath Expression:"
-list1 = root.xpath('//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[3]/table[2]//tr[5]/td[2]/text()')
-print list1
-print len(list1)
+xpathexpression = '//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[3]/table[2]//tr[5]/td[2]/text()'
+print "XPATH Expression: " + xpathexpression
+list1 = root.xpath(xpathexpression)
+print "Value of HTML element identified based on XPATH: " + str(list1[0])
+#print "Number of elements found: " + str(len(list1))
