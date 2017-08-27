9cd71145bae558c9a797701605e43d9ab3bc3f43
Damian Baran <damian.baran@t-systems.com>
2017-08-27 06:25:34 +0200

    added menu

--- html-parser-test.py
+++ html-parser-test.py
@@ -1,6 +1,48 @@
 import urllib2
 import lxml.html
+import re
 
+ans=True
+while ans:
+    print ("""
+    1. 0-500
+    2. 500-1000
+    3. 1000-1500
+    4. 1500-2000
+    5. 2000-2500
+    6. 2500-3000
+    7.Exit/Quit
+    """)
+    ans=raw_input("Vyber obce...")
+    if ans=="1":
+      print("\n Vytvaram zoznam obci 0-500")
+      page = 'https://www.e-obce.sk/zoznam_vsetkych_obci.html?strana=0'
+      ans=None
+    elif ans=="2":
+      print("\n Vytvaram zoznam obci 500-1000")
+      page = 'https://www.e-obce.sk/zoznam_vsetkych_obci.html?strana=500'
+      ans=None
+    elif ans=="3":
+      print("\n Vytvaram zoznam obci 1000-1500")
+      page = 'https://www.e-obce.sk/zoznam_vsetkych_obci.html?strana=1000'
+      ans=None
+    elif ans=="4":
+      print("\n Vytvaram zoznam obci 1500-2000")
+      page = 'https://www.e-obce.sk/zoznam_vsetkych_obci.html?strana=1500'
+      ans=None
+    elif ans=="5":
+      print("\n Vytvaram zoznam obci 2000-2500")
+      page = 'https://www.e-obce.sk/zoznam_vsetkych_obci.html?strana=2000'
+      ans=None
+    elif ans=="6":
+      print("\n Vytvaram zoznam obci 2500-3000")
+      page = 'https://www.e-obce.sk/zoznam_vsetkych_obci.html?strana=2500'
+      ans=None
+    elif ans == "7":
+      print("\n Goodbye")
+      ans=None
+    else:
+      print("\n Not Valid Choice Try again")
 
 obec_exp = '//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[1]/table[1]//tr[1]/td/text()'
 kraj_exp = '//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[3]/table[2]//tr[1]/td[2]/a/text()'
@@ -14,38 +56,75 @@ telefon_exp = '//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[3]/table[1]//tr[3
 email_exp = '//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[3]/table[1]//tr[5]/td[3]/a/text()'
 web_exp = '//*[@id="telo"]/table[1]//tr/td[2]/table//tr/td[3]/table[1]//tr[6]/td[3]/a/text()'
 
+html = urllib2.urlopen(page).read()
 
-url = "https://www.e-obce.sk/obec/trnovo/trnovo.html"
+# Najdi stranky vsetkych obci
+list1 = re.findall('https://+www.e-obce.sk+/obec/+[A-z\-]{1,50}/+.{1,50}.html', html)
 
-html = urllib2.urlopen(url).read()
+for i in list1:
 
-root = lxml.html.fromstring(html)
+    html = urllib2.urlopen(i).read()
 
-obec = root.xpath(obec_exp)
-kraj = root.xpath(kraj_exp)
-okres = root.xpath(okres_exp)
-region = root.xpath(region_exp)
-ico = root.xpath(ico_exp)
-obyvatelov = root.xpath(obyvatelov_exp)
-starosta = root.xpath(starosta_exp)
-mobil = root.xpath(mobil_exp)
-telefon = root.xpath(telefon_exp)
-email = root.xpath(email_exp)
-web = root.xpath(web_exp)
+    root = lxml.html.fromstring(html)
 
-# CSV view
-print obec[0] + "," + kraj[0] + "," + okres[0] + "," + region[0] + "," + obyvatelov[0] + "," + starosta[0] + "," + mobil[0] + "," + telefon[0] + "," + email[0] + "," + web[0]
+    obec = " " * 5
+    kraj = " " * 5
+    okres = " " * 5
+    region = " " * 5
+    ico = " " * 5
+    obyvatelov = " " * 5
+    starosta = " " * 5
+    mobil = " " * 5
+    telefon = " " * 5
+    email = " " * 5
+    web = " " * 5
+
+    obec = root.xpath(obec_exp)
+    kraj = root.xpath(kraj_exp)
+    okres = root.xpath(okres_exp)
+    region = root.xpath(region_exp)
+    ico = root.xpath(ico_exp)
+    obyvatelov = root.xpath(obyvatelov_exp)
+    starosta = root.xpath(starosta_exp)
+    mobil = root.xpath(mobil_exp)
+    telefon = root.xpath(telefon_exp)
+    email = root.xpath(email_exp)
+    web = root.xpath(web_exp)
+
+    if not obec:
+        obec = " " * 5
+    if not kraj:
+        kraj = " " * 5
+    if not okres:
+        okres = " " * 5
+    if not region:
+        region = " " * 5
+    if not obyvatelov:
+        obyvatelov = " " * 5
+    if not starosta:
+        starosta = " " * 5
+    if not mobil:
+        mobil = " " * 5
+    if not telefon:
+        telefon = " " * 5
+    if not email:
+        email = " " * 5
+    if not web:
+        web = " " * 5
+
+    # CSV view
+    print obec[0] + "," + kraj[0] + "," + okres[0] + "," + region[0] + "," + obyvatelov[0] + "," + starosta[0] + "," + mobil[0] + "," + telefon[0] + "," + email[0] + "," + web[0]
 
 
-# Summary view
-# print obec[0]
-# print kraj[0]
-# print okres[0]
-# print region[0]
-# print ico[0]
-# print obyvatelov[0]
-# print starosta[0]
-# print mobil[0]
-# print telefon[0]
-# print email[0]
-# print web[0]
+    # Summary view
+    # print obec[0]
+    # print kraj[0]
+    # print okres[0]
+    # print region[0]
+    # print ico[0]
+    # print obyvatelov[0]
+    # print starosta[0]
+    # print mobil[0]
+    # print telefon[0]
+    # print email[0]
+    # print web[0]
